ce39ecdcd872d22c7cea5f510f45ff0ab29481bc
Andreas Lindeboom <a.s.lindeboom@gmail.com>
2017-04-17 13:03:16 +0200

    Be more verbose in echo

--- build.sh
+++ build.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 set -e
 
-echo "Build image"
+echo "Build image ($REPOSITORY_NAME:$DEFAULT_TAG)"
 docker build -t $REPOSITORY_NAME:$DEFAULT_TAG .
 
 echo "Log in on Docker Hub with user $DOCKER_USERNAME"
 docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
 
 if [[ ! -z $TRAVIS_TAG ]]; then
-  echo "Tag found, tagging image"
+  echo "Tag found, tagging image ($REPOSITORY_NAME:$TRAVIS_TAG)"
   docker tag $REPOSITORY_NAME:master $REPOSITORY_NAME:$TRAVIS_TAG
+  echo "Tag found, tagging image ($REPOSITORY_NAME:latest)"
   docker tag $REPOSITORY_NAME:master $REPOSITORY_NAME:latest
 fi
 
-echo "Push repoitory"
+echo "Push repoitory $REPOSITORY_NAME"
 docker push $REPOSITORY_NAME
 
 echo "Log out on Docker Hub"
