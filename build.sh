fd42c8a38eab6b68e12bd99c601cd9af34a5224d
Andreas Lindeboom <a.s.lindeboom@gmail.com>
2017-04-17 13:01:57 +0200

    Add echo statements to build script

--- build.sh
+++ build.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 set -e
 
+echo "Build image"
 docker build -t $REPOSITORY_NAME:$DEFAULT_TAG .
 
+echo "Log in on Docker Hub with user $DOCKER_USERNAME"
 docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
 
 if [[ ! -z $TRAVIS_TAG ]]; then
+  echo "Tag found, tagging image"
   docker tag $REPOSITORY_NAME:master $REPOSITORY_NAME:$TRAVIS_TAG
   docker tag $REPOSITORY_NAME:master $REPOSITORY_NAME:latest
 fi
 
+echo "Push repoitory"
 docker push $REPOSITORY_NAME
 
+echo "Log out on Docker Hub"
 docker logout
