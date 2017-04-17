fd4004fa69fc7927d9576151ec1cb579da16e9fa
Andreas Lindeboom <a.s.lindeboom@gmail.com>
2017-04-17 13:46:00 +0200

    Move healthcheck to build.sh so that it will check before pushing

--- build.sh
+++ build.sh
@@ -4,6 +4,9 @@ set -e
 echo "Build image ($REPOSITORY_NAME:$DEFAULT_TAG)"
 docker build -t $REPOSITORY_NAME:$DEFAULT_TAG .
 
+echo "Attempting to run ansible --version inside a container based on the built image"
+docker run -it $REPOSITORY_NAME:$DEFAULT_TAG ansible --version
+
 echo "Log in on Docker Hub with user $DOCKER_USERNAME"
 docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
 
--- test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-
-echo "Attempting to run ansible --version inside a container based on the built image"
-docker run -it $REPOSITORY_NAME:$DEFAULT_TAG ansible --version
